c4831cd18768b7ba7d92b7c31c74ea9a55c1a1ed
MarleneBarahona <00118616@uca.edu.sv>
2020-07-04 23:59:42 -0600

    Modificando posiciones y tama;os de las ventanas.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,8 +9,6 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -18,24 +16,29 @@ import java.io.FileReader;
 public class Main extends Application {
 
     Button buttonTXTtoXML, buttonTXTtoJSON;
+    Button b1;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
+        primaryStage.setX(110);
+        primaryStage.setY(50);
         primaryStage.setScene(new Scene(root, 400, 375));
-        //primaryStage.show();
+        primaryStage.show();
         Stage contenido = new Stage();
         TextArea jta1 = new TextArea();
-
+        jta1.setPrefSize(440,300);
         //jta1.getId(idk);
-        Button b1 = new Button("Mostrar contenido");
+        b1 = new Button("Mostrar contenido");
 
         VBox layout = new VBox(5);
         layout.getChildren().addAll(jta1,b1);
-        contenido.setScene(new Scene(layout,450,250));
+        contenido.setScene(new Scene(layout,450,350));
+        contenido.setX(600);
+        contenido.setY(50);
         contenido.show();
         b1.setOnAction(event -> {
-                File archivo = new File("C:/Users/maris/Desktop/Cliente2.txt");
+                File archivo = new File("C:/Users/Marlene/Desktop/idk.xml");
         try {
             BufferedReader leer = new BufferedReader(new FileReader(archivo));
             String linea = leer.readLine();
